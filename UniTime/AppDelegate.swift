aafc2875fd5a9b34544efc25ebc593e64bbae276
Naoki Odajima <dev.nodajima@gmail.com>
2020-05-20 22:42:34 +0900

    Initial Commit

---
+++ UniTime/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UniTime
+//
+//  Created by Naoki Odajima on 2020/05/20.
+//  Copyright © 2020 Naoki Odajima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UniTime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UniTime
+//
+//  Created by Naoki Odajima on 2020/05/20.
+//  Copyright © 2020 Naoki Odajima. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
