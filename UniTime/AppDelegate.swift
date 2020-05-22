92adc28c0b3dc162d51b47ff1eacaacc01b10318
Naoki Odajima <dev.nodajima@gmail.com>
2020-05-23 00:47:22 +0900

    fix: NSMenu + NSViewからNSPopover + ViewControllerに

--- UniTime/AppDelegate.swift
+++ UniTime/AppDelegate.swift
@@ -12,27 +12,43 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     private let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    private let timeConvertVC = TimeConvertViewController(nibName: NSNib.Name("TimeConvertViewController"),
+                                                          bundle: nil)
+    private lazy var popover: NSPopover = {
+        let popover = NSPopover()
+        popover.contentViewController = timeConvertVC
+        popover.behavior = .transient
+        return popover
+    }()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         if let button = statusItem.button {
             button.image = NSImage(named: "StatusbarImage")
             button.imageScaling = .scaleProportionallyDown
+            button.action = #selector(statusItemClicked(_:))
+            button.sendAction(on: [.leftMouseDown])
         }
-        constructMenu()
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    @objc
+    private func statusItemClicked(_ sender: NSStatusBarButton) {
+        NSApp.activate(ignoringOtherApps: true)
+        if popover.isShown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
     }
-
-    private func constructMenu() {
-        let menu = NSMenu()
-        let view: MenuItemView = .instantiateFromNib()
-        let menuItem = NSMenuItem()
-        menuItem.view = view
-        menu.addItem(menuItem)
-        
-        statusItem.menu = menu
+    
+    private func showPopover(_ sender: AnyObject?) {
+        guard let button = statusItem.button else { return }
+        popover.show(relativeTo: button.bounds,
+                     of: button,
+                     preferredEdge: .minY)
+    }
+    
+    private func closePopover(_ sender: AnyObject?) {
+        popover.performClose(sender)
     }
 }
 
---
+++ UniTime/TimeConvertViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TimeConvertViewController.swift
+//  UniTime
+//
+//  Created by Naoki Odajima on 2020/05/23.
+//  Copyright © 2020 Naoki Odajima. All rights reserved.
+//
+
+import Cocoa
+
+class TimeConvertViewController: NSViewController {
+    @IBOutlet private weak var inputTextField: NSTextField!
+    @IBOutlet private weak var convertButton: NSButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
