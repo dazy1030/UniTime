a41624406aad090dda2974e5cc46e8fedc501779
Naoki Odajima <dev.nodajima@gmail.com>
2020-05-26 23:34:41 +0900

    fix: 可読性向上のためのextension作成

---
+++ UniTime/String.extension.swift
@@ -0,0 +1,15 @@
+//
+//  String.extension.swift
+//  UniTime
+//
+//  Created by Naoki Odajima on 2020/05/26.
+//  Copyright © 2020 Naoki Odajima. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var isNotEmpty: Bool {
+        !self.isEmpty
+    }
+}
